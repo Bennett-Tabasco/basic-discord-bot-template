8eb72e945b5c136177a58411583ed619b5f436da
Bennett-Tabasco <83117848+Bennett-Tabasco@users.noreply.github.com>
2021-06-02 23:47:00 +0700

    Update settings.py

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,3 +1,4 @@
+# some of the commands that i commented out is still in test, i'll make an update for them in the future.
 import discord
 import json
 import asyncio
@@ -11,15 +12,17 @@ class Settings(commands.Cog):
 	@commands.Cog.listener()
 	async def on_ready(self):
 		print('Settings is ready.')
-
+	#----------------------------------------------------------
 	#def is_tak(self, context):
-	#	return context.author.id == 724541596976283649
-
+	#	return context.author.id == 'don't copy this line, these things are still in work'
+	#----------------------------------------------------------
+	
 	# Commands
 	@commands.command()
 	async def ping(self, context):
 		await context.send(f'Pong! {round(self.client.latency * 1000)}ms')
-
+		
+	#---------------------------------------------------------------------------------
 	#@commands.command()
 	#@commands.check(is_tak)
 	#async def kick(self, context, member : discord.Member, *, reason=None):
@@ -29,17 +32,18 @@ class Settings(commands.Cog):
 	#@commands.check(is_tak)
 	#async def kick(self, context, member : discord.Member, *, reason=None):
 	#	await member.ban(reason=reason)
+	#----------------------------------------------------------------------------------
 	
+	# clear message command
 	@commands.command()
-	#@commands.check(is_tak)
 	async def clear(self, context, amount=5):
 		await context.channel.purge(limit=amount)
 		await context.send(f'Cleared {amount} message(s).')
 		await asyncio.sleep(3)
 		await context.channel.purge(limit=1)
-
+	
+	# custom prefix command
 	@commands.command(aliases=['prefixSet', 'setPrefix'])
-	#@commands.check(is_tak)
 	async def change_prefix(self, context, prefix):
 		with open('prefixes.json', 'r') as f:
 			prefixes = json.load(f)
