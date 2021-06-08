bb3d0188fe29c140aa0216437261a3d8fd4c697e
Bennett-Tabasco <83117848+Bennett-Tabasco@users.noreply.github.com>
2021-06-08 21:12:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ def get_prefix(client, message):
 		return prefixes[str(message.guild.id)]
 client = commands.Bot(command_prefix=get_prefix, activity=activity, status=discord.Status.idle)
 client.remove_command('help') # remove the current help command and replace it with a new one (check line 51)
+
 # load and set prefix
 @client.event
 async def on_guild_join(guild):
@@ -47,26 +48,6 @@ for filename in os.listdir('./cogs'):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
 #--
-
-# custom help command using embeds (check out more here https://python.plainenglish.io/send-an-embed-with-a-discord-bot-in-python-61d34c711046)
-@client.command(aliases=['help', 'HELP'])
-async def Help(context):	
-	embed=discord.Embed(title="TemplateBot" ,description= f"A list of commands if you don't know the commands (Default prefix is '%')", color=discord.Color.blue())
-	embed.add_field(name="Entertaining stuff.", value="Check out these commands if you're bored.", inline=False)
-	embed.add_field(name="8ball", value="Ask this bot something and it will answer your question", inline=True)
-	embed.add_field(name="HelloWorld", value="Print out 'hello world'", inline=True)
-
-	embed.add_field(name="Settings", value="Used to setting some stuff.", inline=False)
-	embed.add_field(name="mute", value="Used to mute someone", inline=True)
-	embed.add_field(name="unmute", value="Used to unmute someone", inline=True)
-	embed.add_field(name="kick", value="Kick someone out of server (Administrator permission required).", inline=True)
-	embed.add_field(name="ban", value="Ban someone out of server (Administrator permission required).", inline=True)
-	embed.add_field(name="tempban", value="Temp ban someone out of server (Administrator permission required).", inline=True)
-	embed.add_field(name="prefixSet", value="Set the old prefix to a new one.", inline=True)
-	embed.add_field(name="ping", value="Checking for ping.", inline=True)
-	embed.add_field(name="clear", value="Clear messages with a specific amount (Default is 5, Manage messages permission required).", inline=True)
-	
-	await context.send(embed=embed)
 
 #this will handle 'command not found' error
 @client.event
