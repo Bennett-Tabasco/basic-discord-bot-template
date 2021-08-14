4e90b404f274a28ca4221173463e18419588291f
Bennett-Tabasco <83117848+Bennett-Tabasco@users.noreply.github.com>
2021-08-14 14:24:51 +0700

    Add files via upload

---
+++ Discord PY (Rewrite Ver)/README.md
@@ -0,0 +1,23 @@
+# Basic Discord Bot Template Rewritten
+
+This is a rewrite version of the original [discord bot template](https://github.com/Bennett-Tabasco/basic-discord-bot-template) written in discord.py
+
+For full documentation, please check `extensions/document/document.txt`
+# Packages & Software
+
+### Python Packages used to run the bot (check `extensions/packages/requirements.txt` for installation details):
+
+* [discord.py](https://discordpy.readthedocs.io/en/latest/index.html) (third-party package)
+* [dotenv](https://pypi.org/project/python-dotenv/) (third-party package)
+* [os](https://www.geeksforgeeks.org/os-module-python-examples/) (built-in package)
+* [itertools](https://docs.python.org/3/library/itertools.html) (built-in package)
+* [asyncio](https://docs.python.org/3/library/asyncio.html) (built-in package)
+* [PyNaCl](https://pypi.org/project/PyNaCl/) (third-party packages)
+* [youtube-dl](https://pypi.org/project/youtube_dl/) (third-party package)
+* [json](https://docs.python.org/3/library/json.html) (built-in packages)
+
+* [FFmpeg](https://www.ffmpeg.org/download.html#build-windows) (software)
+# Bugs & Issues
+### Currently, there are no bugs or issues occured yet.
+<br>
+
---
+++ Discord PY (Rewrite Ver)/cogs/config.py
@@ -0,0 +1,72 @@
+import discord
+import json
+import asyncio
+import os
+from discord.ext import commands
+
+# Path to JSON file
+jsonPath = f"{os.getcwd()}\\extensions\\client-data\\prefixes.json"
+
+class Settings(commands.Cog): 
+	def __init__(self, client):
+		self.client = client
+
+	# Event
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print('Settings is ready.')
+
+	# Commands
+	@commands.command()
+	async def ping(self, ctx):
+		await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+	
+	@commands.command()
+	@commands.has_permissions(manage_messages=True)
+	async def clear(self, ctx, amount=5): # <-- clear message command
+		if amount > 150:
+			await ctx.send("Amount of messages to delete must be smaller than 150")
+			return
+		
+		await ctx.channel.purge(limit=amount+1) 
+		await ctx.send(f'Cleared {amount} message(s).')
+		await asyncio.sleep(3)
+		await ctx.channel.purge(limit=1)
+	
+
+	@commands.command(aliases=['prefixSet']) 
+	@commands.has_permissions(administrator=True)
+	async def change_prefix(self, ctx, prefix):
+		valid_prefix = ['!', '@', '#', '$', '%', '^', '&', '*','-', '_', '+', '=', '~', '>', '<', '.', ',', '?', '\\', '`', '|', '/']
+		if prefix not in valid_prefix:
+			await ctx.send('That is not a valid prefix!')
+			return
+		if prefix in valid_prefix:
+			with open(jsonPath, 'r') as f:
+				prefixes = json.load(f)
+
+			prefixes["guilds"][ctx.guild.name] = {}
+			prefixes["guilds"][ctx.guild.name]["id"] = ctx.guild.id
+			prefixes["guilds"][ctx.guild.name]["prefix"] = prefix
+
+			with open(jsonPath, 'w') as f:
+				json.dump(prefixes, f, indent=4)
+			await ctx.send(f'**Prefix changed to** `{prefix}`')
+	
+	@clear.error
+	async def clear_error(self, ctx, error):
+		if isinstance(error, commands.MissingPermissions):
+			await ctx.send("You don't have permissions to run this command.")
+		elif isinstance(error, commands.BotMissingPermissions):
+			await ctx.send("I don't have the required permission, please give me administrator permission in order to run this command.")
+			
+	@change_prefix.error
+	async def change_prefix_error(self, ctx, error):
+		if isinstance(error, commands.MissingPermissions):
+			await ctx.send("You don't have permissions to run this command.")
+		elif isinstance(error, commands.MissingRequiredArgument):
+			await ctx.send("Please specify a character for a new prefix.")
+
+def setup(client):
+	client.add_cog(Settings(client))
+
